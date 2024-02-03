39cbe5a397932d42a7d5d4f0b1c19288e19992d3
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-02-03 01:19:34 -0300

    Add project files.

---
+++ LeanworkRecursosHumano.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeanworkRecursosHumano.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
