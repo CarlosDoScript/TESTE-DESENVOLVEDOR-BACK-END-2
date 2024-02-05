b3e02d61314d1cb75b25f8ad3f4f2d067f31778e
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-02-04 21:40:24 -0300

    Alterando o e arrumando o layout da tela de usuarios do git hub

--- LeanworkRecursosHumano.WEB/Startup.cs
+++ LeanworkRecursosHumano.WEB/Startup.cs
@@ -55,7 +55,7 @@ namespace LeanworkRecursosHumano.WEB
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=UsersGithub}/{action=Index}/{id?}");
             });
         }
     }
